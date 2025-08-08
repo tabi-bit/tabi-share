82078a2e8ad07102615256b81025de82ad09a8da
kuu13580 <13580kuu@gmail.com>
2025-08-08 12:01:45 +0000

    add: gemini cliのインストール

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-chown -R vscode:vscode ~
+sudo chown -R vscode:vscode ~
 
 ./scripts/setup.sh
 
 # Claude Code
 npm install -g @anthropic-ai/claude-code
+
+# Gemini CLI
+npm install -g @google/gemini-cli

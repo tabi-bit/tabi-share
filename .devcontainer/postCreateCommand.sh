0415ee88e7b0d14e51dda6c20f8e6de81ea6d55f
kuu13580 <13580kuu@gmail.com>
2025-08-08 16:23:55 +0000

    add: serena を導入

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,6 +7,7 @@ sudo chown -R vscode:vscode ~
 # Claude Code
 npm install -g @anthropic-ai/claude-code
 uv tool install claude-monitor
+claude mcp add serena -- uvx --from git+https://github.com/oraios/serena serena start-mcp-server --context ide-assistant --project $(pwd)
 
 # Gemini CLI
 npm install -g @google/gemini-cli

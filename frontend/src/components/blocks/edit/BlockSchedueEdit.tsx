5ac4d08692fdf4d008f3355ef91a9e37695640f2
Kohtaroh Sakaue <spikekohtaroh@gmail.com>
2025-09-24 16:13:50 +0000

    add: implement BlockScheduleEdit component and associated stories; add new SVG icons

--- frontend/src/components/blocks/edit/BlockSchedueEdit.tsx
+++
@@ -1,3 +0,0 @@
-export function BlockScheduleEdit() {
-  return <div>テスト</div>;
-}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './App.tsx';
+import 'dayjs/locale/ja';
+import dayjs from 'dayjs';
 
+dayjs.locale('ja');
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(

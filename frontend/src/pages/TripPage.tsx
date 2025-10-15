1461998c784185ee897dfd6424c7d4ce9b8889a7
kuu13580 <13580kuu@gmail.com>
2025-10-15 13:58:54 +0000

    fix: demoデータ改変

--- frontend/src/pages/TripPage.tsx
+++ frontend/src/pages/TripPage.tsx
@@ -1,67 +1,7 @@
 import { useRef, useState } from 'react';
 import { Header } from '@/components/Header';
 import { Timeline } from '@/components/timeline/Timeline';
-import type { Block, Page } from '@/types';
-import type { Trip } from '@/types/trip';
-
-const demoBlocks: Block[] = [
-  {
-    id: '1',
-    type: 'schedule',
-    title: '予定のタイトル（テンプレート）',
-    startTime: new Date(2024, 0, 1, 12, 0),
-    endTime: new Date(2024, 0, 1, 14, 0),
-    details: `detail detail
-• detail detail
-リンク`.repeat(20),
-  },
-  {
-    id: '2',
-    type: 'transportation',
-    transportationType: 'car',
-    title: '車移動',
-    startTime: new Date(2024, 0, 1, 15, 0),
-    endTime: new Date(2024, 0, 1, 16, 0),
-  },
-  {
-    id: '3',
-    type: 'schedule',
-    title: '予定のタイトル（テンプレート）',
-    startTime: new Date(2024, 0, 1, 17, 0),
-    endTime: new Date(2024, 0, 1, 19, 0),
-  },
-  {
-    id: '4',
-    type: 'transportation',
-    transportationType: 'car',
-    title: '車移動',
-    startTime: new Date(2024, 0, 1, 19, 0),
-    endTime: new Date(2024, 0, 1, 20, 0),
-    details: `detail detail
-• detail detail
-リンク`.repeat(20),
-  },
-];
-
-const demoPages: Page[] = [
-  {
-    id: 'page1',
-    title: '1日目',
-  },
-  {
-    id: 'page2',
-    title: '2日目',
-  },
-  {
-    id: 'page3',
-    title: '3日目',
-  },
-];
-
-const demoTrip: Trip = {
-  id: 'trip1',
-  title: 'サンプル旅行',
-};
+import { demoBlocks1, demoBlocks2, demoPages, demoTrip } from './test-data';
 
 const TripPage = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
@@ -81,7 +21,7 @@ const TripPage = () => {
         scrollContainerRef={scrollContainerRef}
       />
       <div className='flex h-full w-full max-w-3xl grow flex-col items-center'>
-        <Timeline blocks={demoBlocks} type='view' className='pb-4' />
+        <Timeline blocks={selectedPageId === 'page1' ? demoBlocks1 : demoBlocks2} type='view' className='pb-4' />
       </div>
     </div>
   );
---
+++ frontend/src/pages/test-data.ts
@@ -0,0 +1,136 @@
+import type { Block, Page } from '@/types';
+import type { Trip } from '@/types/trip';
+
+export const demoBlocks2: Block[] = [
+  {
+    id: '1',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 12, 0),
+    endTime: new Date(2024, 0, 1, 14, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+  {
+    id: '2',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 1, 15, 0),
+    endTime: new Date(2024, 0, 1, 16, 0),
+  },
+  {
+    id: '3',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 17, 0),
+    endTime: new Date(2024, 0, 1, 19, 0),
+  },
+  {
+    id: '4',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 1, 19, 0),
+    endTime: new Date(2024, 0, 1, 20, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+];
+
+export const demoBlocks1: Block[] = [
+  {
+    id: '1',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 12, 0),
+    endTime: new Date(2024, 0, 1, 14, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+  {
+    id: '2',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 1, 15, 0),
+    endTime: new Date(2024, 0, 1, 16, 0),
+  },
+  {
+    id: '3',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 17, 0),
+    endTime: new Date(2024, 0, 1, 19, 0),
+  },
+  {
+    id: '4',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 1, 19, 0),
+    endTime: new Date(2024, 0, 1, 20, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+  {
+    id: '5',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 20, 0),
+    endTime: new Date(2024, 0, 1, 22, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+  {
+    id: '6',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 1, 22, 0),
+    endTime: new Date(2024, 0, 1, 23, 0),
+  },
+  {
+    id: '7',
+    type: 'schedule',
+    title: '予定のタイトル（テンプレート）',
+    startTime: new Date(2024, 0, 1, 23, 0),
+    endTime: new Date(2024, 0, 2, 1, 0),
+  },
+  {
+    id: '8',
+    type: 'transportation',
+    transportationType: 'car',
+    title: '車移動',
+    startTime: new Date(2024, 0, 2, 1, 0),
+    endTime: new Date(2024, 0, 2, 2, 0),
+    details: `detail detail
+• detail detail
+リンク`.repeat(20),
+  },
+];
+
+export const demoPages: Page[] = [
+  {
+    id: 'page1',
+    title: '1日目',
+  },
+  {
+    id: 'page2',
+    title: '2日目',
+  },
+  {
+    id: 'page3',
+    title: '3日目',
+  },
+];
+
+export const demoTrip: Trip = {
+  id: 'trip1',
+  title: 'サンプル旅行',
+};

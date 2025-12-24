90f4824127fc9d94ce52c386fa76c50f4a19052b
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2025-12-24 17:23:47 +0000

    add: zodの導入とバリデーションスキーマの追加

--- frontend/src/types/block.ts
+++ frontend/src/types/block.ts
@@ -1,21 +1,29 @@
-type BaseBlock = {
-  id: string;
-  type: 'schedule' | 'transportation';
-  title: string;
-  startTime: Date;
-  endTime: Date;
-  details?: string;
-};
+import { z } from 'zod';
 
-export type Block = ScheduleBlock | TransportationBlock;
+export const TransportationTypeEnum = z.enum(['car', 'bicycle', 'walk', 'ship', 'train', 'bus', 'flight']);
 
-export type ScheduleBlock = BaseBlock & {
-  type: 'schedule';
-};
+export const BaseBlockSchema = z.object({
+  id: z.string(),
+  type: z.enum(['schedule', 'transportation']),
+  title: z.string(),
+  startTime: z.date(),
+  endTime: z.date(),
+  details: z.string().optional(),
+});
 
-export type TransportationBlock = BaseBlock & {
-  type: 'transportation';
-  transportationType: TransportationType;
-};
+export const ScheduleBlockSchema = BaseBlockSchema.extend({
+  type: z.literal('schedule'),
+});
 
-export type TransportationType = 'car' | 'bicycle' | 'walk' | 'ship' | 'train' | 'bus' | 'flight';
+export const TransportationBlockSchema = BaseBlockSchema.extend({
+  type: z.literal('transportation'),
+  transportationType: TransportationTypeEnum,
+});
+
+export const BlockSchema = z.discriminatedUnion('type', [ScheduleBlockSchema, TransportationBlockSchema]);
+
+export type BaseBlock = z.infer<typeof BaseBlockSchema>;
+export type Block = z.infer<typeof BlockSchema>;
+export type ScheduleBlock = z.infer<typeof ScheduleBlockSchema>;
+export type TransportationBlock = z.infer<typeof TransportationBlockSchema>;
+export type TransportationType = z.infer<typeof TransportationTypeEnum>;
--- frontend/src/types/trip.ts
+++ frontend/src/types/trip.ts
@@ -1,5 +1,9 @@
-export type Trip = {
-  id: string;
-  title: string;
-  peopleNum?: number;
-};
+import { z } from 'zod';
+
+export const TripSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  peopleNum: z.number().optional(),
+});
+
+export type Trip = z.infer<typeof TripSchema>;

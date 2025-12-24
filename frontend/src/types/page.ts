2dd8d997646af1ee1a1607091832dc10784367b3
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2025-12-24 17:23:47 +0000

    add: zodの導入とバリデーションスキーマの追加

--- frontend/src/types/page.ts
+++ frontend/src/types/page.ts
@@ -1,5 +1,9 @@
-export type Page = {
-  id: string;
-  title: string;
-  details?: string;
-};
+import { z } from 'zod';
+
+export const PageSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  details: z.string().optional(),
+});
+
+export type Page = z.infer<typeof PageSchema>;

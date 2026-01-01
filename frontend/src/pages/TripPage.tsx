f10da6961bba48dd2d54d85ebf4f5c41aa5b0408
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2026-01-01 17:31:11 +0000

    add: URLIDからtripを取得

--- frontend/src/pages/TripPage.tsx
+++ frontend/src/pages/TripPage.tsx
@@ -1,21 +1,29 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Header } from '@/components/Header';
 import { Timeline } from '@/components/timeline/Timeline';
 import { useBlocks } from '@/hooks/useBlocks';
 import { usePages } from '@/hooks/usePages';
-import { useTrip } from '@/hooks/useTrips';
+import { useTripByUrlId } from '@/hooks/useTrips';
 import type { Page } from '@/types';
 
 const TripPage = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const [selectedPageId, setSelectedPageId] = useState<Page['id']>();
+  const { urlId } = useParams<{ urlId: string }>();
 
-  const { trip, error: tripError, isLoading: isTripLoading } = useTrip(1);
+  const { trip, error: tripError, isLoading: isTripLoading } = useTripByUrlId(urlId ?? null);
   const { pages, error: pagesError, isLoading: isPagesLoading } = usePages(trip?.id ?? null);
   const { blocks, error: blocksError, isLoading: isBlocksLoading } = useBlocks(selectedPageId ?? null);
 
   const isLoading = isTripLoading || isPagesLoading || trip == null || pages == null;
   const isError = pagesError || trip == null;
+
+  useEffect(() => {
+    if (selectedPageId == null && pages != null) {
+      setSelectedPageId(pages[0].id);
+    }
+  }, [pages, selectedPageId]);
 
   return (
     <>

cef70f32db560b6bba7e5c10afbae08b879a8b8f
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2026-01-01 16:27:31 +0000

    add: APIのスネークケースをローワーキャメルケースに変換する

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,4 +1,23 @@
 import axios from 'axios';
+import _ from 'lodash';
+
+// スネークケースのキーをローワーキャメルケースに変換するヘルパー関数
+const keysToCamel = (obj: any): any => {
+  if (_.isObject(obj) && !_.isArray(obj)) {
+    return _.reduce(
+      obj,
+      (acc, value, key) => {
+        acc[_.camelCase(key)] = keysToCamel(value);
+        return acc;
+      },
+      {} as any
+    );
+  }
+  if (_.isArray(obj)) {
+    return _.map(obj, (item: any) => keysToCamel(item));
+  }
+  return obj;
+};
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -6,5 +25,18 @@ export const apiClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+apiClient.interceptors.response.use(
+  response => {
+    // 応答データがオブジェクトまたは配列の場合にキーを変換
+    if (response.data && (typeof response.data === 'object' || Array.isArray(response.data))) {
+      response.data = keysToCamel(response.data);
+    }
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export const fetcher = (url: string) => apiClient.get(url).then(res => res.data);

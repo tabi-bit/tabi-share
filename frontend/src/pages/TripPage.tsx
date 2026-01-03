d2e8664f77ef76d08361fc9c06faf754ec55927d
kuu13580 <46004336+kuu13580@users.noreply.github.com>
2026-01-03 05:13:39 +0000

    fix: TripPageを分離

--- frontend/src/pages/TripPage.tsx
+++ frontend/src/pages/TripPage.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Header } from '@/components/Header';
-import { Timeline } from '@/components/timeline/Timeline';
-import { useBlocks } from '@/hooks/useBlocks';
 import { usePages } from '@/hooks/usePages';
 import { useTripByUrlId } from '@/hooks/useTrips';
 import type { Page } from '@/types';
+import { ViewTripLayout } from './TripPage/ViewTripLayout';
 
 const TripPage = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
@@ -14,45 +13,49 @@ const TripPage = () => {
 
   const { trip, error: tripError, isLoading: isTripLoading } = useTripByUrlId(urlId ?? null);
   const { pages, error: pagesError, isLoading: isPagesLoading } = usePages(trip?.id ?? null);
-  const { blocks, error: blocksError, isLoading: isBlocksLoading } = useBlocks(selectedPageId ?? null);
 
   const isLoading = isTripLoading || isPagesLoading || trip == null || pages == null;
-  const isError = pagesError || trip == null;
+  const isError = tripError || pagesError;
 
   useEffect(() => {
-    if (selectedPageId == null && pages != null) {
+    if (selectedPageId == null && pages != null && pages.length > 0) {
       setSelectedPageId(pages[0].id);
     }
   }, [pages, selectedPageId]);
 
+  if (isLoading) {
+    return <div>Loading Trip ...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        {tripError && `Trip Loading Error: ${String(tripError)}`}
+        {pagesError && `Pages Loading Error: ${String(pagesError)}`}
+      </div>
+    );
+  }
+
   return (
     <>
-      {isLoading && <div>Loading Trip ...</div>}
-      {!(isLoading || isError) && (
+      {trip && pages && (
         <div
           ref={scrollContainerRef}
           className='flex h-screen w-full flex-col items-center justify-between gap-4 overflow-auto'
         >
           {selectedPageId != null && (
-            <>
-              <Header
-                selectedPageId={selectedPageId}
-                pages={pages}
-                onSelectPage={setSelectedPageId}
-                trip={trip}
-                mode='view'
-                scrollContainerRef={scrollContainerRef}
-              />
-              <div className='flex h-full w-full max-w-3xl grow flex-col items-center'>
-                {!isBlocksLoading && blocks && <Timeline blocks={blocks} type='view' className='pb-4' />}
-              </div>
-            </>
+            <Header
+              selectedPageId={selectedPageId}
+              pages={pages}
+              onSelectPage={setSelectedPageId}
+              trip={trip}
+              mode='view'
+              scrollContainerRef={scrollContainerRef}
+            />
           )}
+          {selectedPageId != null && <ViewTripLayout selectedPageId={selectedPageId} />}
         </div>
       )}
-      {tripError && <div>Trip Loading Error: {String(tripError)}</div>}
-      {pagesError && <div>Trip Loading Error: {String(pagesError)}</div>}
-      {blocksError && <div>Trip Loading Error: {String(blocksError)}</div>}
     </>
   );
 };
---
+++ frontend/src/pages/TripPage/ViewTripLayout.tsx
@@ -0,0 +1,25 @@
+import { Timeline } from '@/components/timeline/Timeline';
+import { useBlocks } from '@/hooks/useBlocks';
+import type { Page } from '@/types';
+
+type ViewTripLayoutProps = {
+  selectedPageId: Page['id'];
+};
+
+const ViewTripLayout = ({ selectedPageId }: ViewTripLayoutProps) => {
+  const { blocks, error: blocksError, isLoading: isBlocksLoading } = useBlocks(selectedPageId ?? null);
+
+  return (
+    <>
+      {isBlocksLoading && <div>Loading Blocks ...</div>}
+      {!isBlocksLoading && blocks && (
+        <div className='flex h-full w-full max-w-3xl grow flex-col items-center'>
+          <Timeline blocks={blocks} type='view' className='pb-4' />
+        </div>
+      )}
+      {blocksError && <div>Blocks Loading Error: {String(blocksError)}</div>}
+    </>
+  );
+};
+
+export { ViewTripLayout };

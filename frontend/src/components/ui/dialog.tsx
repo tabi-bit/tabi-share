b07b92951c94f289d1721e23b720766c5049c291
kuu13580 <13580kuu@gmail.com>
2026-02-11 19:45:52 +0000

    fix: ダイアログのautoFocusを無効化

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -46,6 +46,9 @@ function DialogContent({
       <DialogOverlay />
       <DialogPrimitive.Content
         data-slot='dialog-content'
+        onOpenAutoFocus={e => {
+          e.preventDefault();
+        }}
         className={cn(
           'data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 flex max-h-[80vh] w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] flex-col gap-4 overflow-hidden rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in sm:max-w-lg',
           className

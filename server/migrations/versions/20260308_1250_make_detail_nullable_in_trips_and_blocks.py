b6bf932a271a447cbefc4190b730e56b711139d4
Kohtaroh Sakaue <spikekohtaroh@gmail.com>
2026-03-08 13:10:48 +0900

    fix: マイグレーションのdown_revisionをb1c2d3e4f5a6に修正しheadを単一化

--- server/migrations/versions/20260308_1250_make_detail_nullable_in_trips_and_blocks.py
+++ server/migrations/versions/20260308_1250_make_detail_nullable_in_trips_and_blocks.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'ee1707608398'
-down_revision: Union[str, Sequence[str], None] = 'a4e278391af4'
+down_revision: Union[str, Sequence[str], None] = 'b1c2d3e4f5a6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

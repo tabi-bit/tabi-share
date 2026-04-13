a66269e186b8b87099381f9cd961631c60bfef81
kuu13580 <13580kuu@gmail.com>
2026-04-13 13:04:47 +0000

    add: GoogleMaps APIのAPIキー設定

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_APP_ENV: 'production' | 'staging' | 'local';
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_GOOGLE_MAPS_API_KEY: string;
 }
 
 interface ImportMeta {
